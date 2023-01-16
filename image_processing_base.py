884a2b25174ffb78a199bf6c0d8960358cb76369
arzuyildiz8 <arzu.yildiz@coino.com>
2023-01-16 14:06:51 +0300

    histogram add

--- image_processing_base.py
+++ image_processing_base.py
@@ -550,10 +550,52 @@ def gradients():
     plt.axis("off")
     plt.title("Laplacian")
 
-gradients()
+#gradients()
     
+
+def histogram():
     
+    """Histogram renk yoğunluğunu grafik olarak gözlemlememize olanak sağlar."""
     
+    # mask
+    golden_gate = cv2.imread("goldenGate.jpg") 
+    golden_gate_vis = cv2.cvtColor(golden_gate, cv2.COLOR_BGR2RGB)
+    plt.figure()
+    plt.imshow(golden_gate_vis)    
+     
+    color = ("b", "g", "r")
+    plt.figure()
+    for i, c in enumerate(color):
+        print([i])
+        
+        img_hist = cv2.calcHist([golden_gate], channels = [i], mask = None, histSize = [256], ranges = [0,256]) # color channel order bgr bu nedenle channel sıfır yazıyoruz.
+        plt.figure()
+        plt.plot(img_hist,  color = c)
+    
+   
+    def histogram_equalization():
+        """histogram equalization (histogram eşitleme):
+            Kontrastı artırır ve detayları belirginleştirir."""
+        
+            
+        img = cv2.imread('hist_equ.jpg',0)
+        plt.figure()
+        plt.imshow(img, cmap = "gray")
+        
+        img_hist = cv2.calcHist([img], channels = [0], mask = None, histSize = [256], ranges = [0,256])
+        
+        plt.figure()
+        plt.plot(img_hist)
+        
+        eq_img = cv2.equalizeHist(img)
+        plt.figure()
+        plt.imshow(eq_img, cmap = "gray")
+        
+        eq_img_hist = cv2.calcHist([eq_img], channels = [0], mask = None, histSize = [256], ranges = [0,256])
+        plt.figure()
+        plt.plot(eq_img_hist)
+    histogram_equalization()
+histogram()
     
     
     
